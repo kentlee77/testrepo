f46b918449572678cfef8c7b48729328a5d2a92c
Kent Lee <122420587+kentlee77@users.noreply.github.com>
2024-05-31 14:42:45 +0900

    Add files via upload

---
+++ basicmath.py
@@ -0,0 +1,9 @@
+squares = []
+for value in range(0,21):
+   sq = value ** 2 
+   squares.append(sq) 
+   print(f"{value}^2 =  {sq}")   
+
+fibs = []
+for val in range(0,10):
+   val2 = val 
---
+++ hello.py
@@ -0,0 +1,26 @@
+# Install IDLE, Visual Studio Code, and/or Sublime editors
+
+message1 = "Hellooo, Python world!"
+print(message1)
+
+message2 = "Hello and goodbye, world!"
+print(message2)
+
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+# Practice 
+fnom = "nikola"
+lnom = "tesla"
+fullnom = f"{fnom} {lnom}"
+msg= "Are you interested in learning...?"
+lg1 = "Latin"
+lg2 = "French "
+lg3 = "Klingon "
+print(f"Hello, {fullnom.title()}!\n{msg}\n\t{lg1.rstrip()}\n\t{lg2.rstrip()}\n\t{lg3.rstrip()}")
+
+print(2 ** 10)
+
+import this 
